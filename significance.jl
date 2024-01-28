d1c341b677ee706436c71653458c755271e468b1
John Bjørnar Bremnes <j.b.bremnes@met.no>
2024-01-28 16:37:35 +0100

    update on significance testing

--- significance.jl
+++ significance.jl
@@ -1,11 +1,11 @@
-#  testing statistical significance using
+#  testing statistical significance of BQN calibrated forecasts using
 #    - Diebold-Mariano hypothesis test of equal CRPS for each lead site and lead time
 #    - Benjamini and Hochberg (1995) approach to aggregate
 #
-#   julia --project=./ --threads=3
+#   julia --project=./   # 64 GB memory is probably required
 #   
 
-using DataFrames, CSV, Statistics, Distributions#, Dates
+using DataFrames, CSV, Statistics, Distributions, NamedArrays
 
 
 include("loss_functions.jl")
@@ -20,7 +20,7 @@ function get_forecasts(prm, model)
     experiments = [string("m",i,"b",j) for j in 1:3, i in 1:3][:]
     files = string.("data/qts_bqn_$(prm)_+60_$(model)_", experiments, ".csv")
     df = CSV.read.(files, DataFrame)
-    return df #reduce(vcat, df)
+    return df 
 end
 
 
@@ -52,6 +52,7 @@ function compute_stats(df, df2; level = 0.05)
     scr.dm = @. sqrt(scr.n) * (scr.crps1_mean - scr.crps2_mean) / scr.diff2_mean
     scr.p_value = cdf.(Normal(0,1), scr.dm)
     scr.bh_5pct = bh_test(scr.p_value)
+    
     return scr
 end
 
@@ -67,6 +68,8 @@ function significance_stats()
                     df2 = get_forecasts(p, m2)
                     scores[i, i2, ip] = compute_stats(df, df2; level = 0.05)
                     println("  $(m) vs $(m2): $(round(mean(scores[i,i2,ip].bh_5pct), digits=2))")
+                    df2 = nothing
+                    GC.gc()
                 end
             end
         end
@@ -75,4 +78,46 @@ function significance_stats()
 end
 
 
-@time stats = significance_stats()
+@time stats = significance_stats()  # ~3 hours
+JLD2.@save "data/significance_cal.jld2" models prm stats
+
+best = NamedArray(zeros(6,6,2), (models, models, prm))
+for p in axes(stats, 3)
+    for m in axes(stats, 1)
+        for m2 in axes(stats, 2)
+            if m != m2
+                best[m,m2,p] = round(mean(stats[m,m2,p].bh_5pct)*100, digits=1)
+            end
+        end
+    end
+end
+
+         
+
+
+
+#=
+:, :, C=t2] =
+A ╲ B │ pangu   hres    ens   meps   ens0  meps0
+──────┼─────────────────────────────────────────
+pangu │   0.0   33.6   28.4   14.4   42.5   20.3
+hres  │  27.9    0.0   23.7    9.3   44.3   17.6
+ens   │  39.1   46.1    0.0   16.1   91.5   26.1
+meps  │  45.5   49.2   40.1    0.0   53.6   72.2
+ens0  │  20.9   23.1    1.6    6.3    0.0   11.9
+meps0 │  28.9   32.2   24.8    3.6   36.3    0.0
+
+[:, :, C=ws10] =
+A ╲ B │ pangu   hres    ens   meps   ens0  meps0
+──────┼─────────────────────────────────────────
+pangu │   0.0   27.5   15.2    7.0   32.8   20.6
+hres  │  28.9    0.0   18.1    9.2   36.6   24.9
+ens   │  50.4   54.5    0.0   19.3   91.6   40.5
+meps  │  54.8   52.1   42.3    0.0   56.4   88.2
+ens0  │  27.7   31.0    2.8    8.3    0.0   23.6
+meps0 │  23.9   24.4   15.9    0.5   27.5    0.0
+
+=#
+
+
+
